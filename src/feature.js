aced6de1711066500de1bc36b2b0a47218fa189b
Akinwoleola Opeyemi <akinwoleolaopeyemi@gmail.com>
2015-05-20 10:41:15 +0100

    Update feature.js in src and install firebase,angular and angular fire in index.html

---
+++ src/feature.js
@@ -0,0 +1,18 @@
+var app = angular.module("Jobtronics", ["firebase"]);
+app.factory("UserStats"["$firebaseObject",
+  function($firebaseObject) {
+  	return function(Username){    
+  	// ...
+    	var ref = new Firebase("https://jobtronics.firebaseio.com");
+    	var userRef = ref.child("Username")
+
+    	return $firebaseObject(userRef);
+	}
+  }
+]);
+
+app.controller("UserCtrl", ["$scope", "UserStats"
+	function($scope, UserStats){
+		UserStats("")$.bindTo($scope, "userstats");
+	}
+]);
